99680047353ad31f686b54560750529807bf691e
David Moench <domoench@gmail.com>
2020-04-12 11:44:29 -0400

    Experiment with dynamically updating the circle pack data hierarchy.
    Proves we can modify the data hierarchy on the fly.
    This would allow us do thing like:
    - Only render the individual commitments when the parent is zoomed in
      on.
    
    Important:
    - Must use a data join key so the DOM elements can consistently map to
      the data hierarchy when things change dynamically.
    - Figured out how do transitions

--- frontend/src/components/visualization/PackedCircles.js
+++ frontend/src/components/visualization/PackedCircles.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { select, event } from 'd3-selection';
 import { pack, hierarchy } from 'd3-hierarchy';
 import { zoom } from 'd3-zoom';
+import { v4 as uuidv4 } from 'uuid';
 
 // Generate dummy hierarchichal data
 // Hierarchy: Country -> Postal Code -> Commitment Type -> Individual commitments
@@ -14,15 +15,21 @@ const generateHierarchicalData = () => {
     const randomPostalCodes = postalCodes.filter(pc => Math.random() > 0.6);
 
     return {
+      // TODO: Add ids to all data nodes.
+      // Try data join keys so we can add/remove nodes => Dynamic aggregation on zoom out
+      id: uuidv4(),
+      visible: true,
       name: countryCode,
       children: randomPostalCodes.map(pc => generatePostalNode(pc)),
     };
   };
 
   const generatePostalNode = (postalCode) => {
-    const randomCommitments = postalCodes.filter(pc => Math.random() > 0.8);
+    const randomCommitments = commitments.filter(pc => Math.random() > 0.8);
 
     return {
+      id: uuidv4(),
+      visible: true,
       name: postalCode,
       children: randomCommitments.map(c => generateCommitmentNode(c)),
     };
@@ -32,33 +39,46 @@ const generateHierarchicalData = () => {
     const numCommitments = Math.ceil(Math.random() * 50); // TODO this looks bad when numbers get high
     const commitments = new Array(numCommitments);
     for (let i = 0; i < numCommitments; ++i) {
-      commitments[i] = { name: '🌱' };
+      commitments[i] = {
+        id: uuidv4(),
+        name: '🌱'
+      };
     }
 
     return {
+      id: uuidv4(),
+      visible: true,
       name: commitment,
       children: commitments,
     };
   }
 
   return {
+    id: uuidv4(),
+    visible: true,
     name: 'earth',
     children: countryCodes.map(cc => generateCountryNode(cc)),
   };
 };
 
-const renderPackedCircleGraph = (data, width, height) => {
+const renderPackedCircleGraph = (rootNode, width, height) => {
   const packLayout = pack().size([width, height]).padding(4).size([width - 2, height - 2]);
-  const rootNode = hierarchy(data).sum(d => 1);
   packLayout(rootNode);
 
-  const svg = select('.packed-circles-viz');
-  svg.selectAll("*").remove(); // TODO Hack to deal with react re-renders
-  const g = svg.append('g');
+  const svg = select('.packed-circles-viz')
+    .attr('text-anchor', 'middle');
+  const g = svg.select('g');
 
   const node = g.selectAll('circle')
-    .data(rootNode.descendants().slice(1))
-    .enter()
+    .data(rootNode.descendants().slice(1), d => d.data.id)
+
+  node.transition()
+    .duration(300)
+    .attr("cx", function(d) { return d.x; })
+    .attr("cy", function(d) { return d.y; })
+    .attr("r", function(d) { return d.r; });
+
+  node.enter()
     .append('circle')
       .attr('cx', function(d) { return d.x; })
       .attr('cy', function(d) { return d.y; })
@@ -67,31 +87,46 @@ const renderPackedCircleGraph = (data, width, height) => {
       .style('opacity', 0.3)
       .style('stroke', '#207068')
 
+  node.exit().remove();
+
   /*
   const text = g.selectAll("text")
     .data(rootNode.descendants().slice(1))
     .enter().append("text")
       .attr("class", "label")
-      .attr('x', function(d) { return d.x + d.r * 0.8; })
-      .attr('y', function(d) { return d.y - d.r * 0.8; })
-      .style("fill-opacity", function(d) { return d.parent === rootNode ? 1 : 0; })
+      .attr('x', function(d) { return d.x; })
+      .attr('y', function(d) { return d.y; })
+      .style("fill-opacity", function(d) { return d.parent === rootNode ? 0.1 : 0; })
       .style("display", function(d) { return d.parent === rootNode ? "inline" : "none"; })
-      .text(function(d) { return d.data.name; });
+      .text(function(d) { return d.data.name; })
+      .style("font-size", function(d) { return Math.min(2 * d.r, (2 * d.r - 8) / this.getComputedTextLength() * 18) + "px"; })
+      .attr("dy", ".35em");
+
+  text.exit().remove();
   */
 }
 
 export default () => {
   const data = generateHierarchicalData(); // TODO
+  const rootNode = hierarchy(data).sum(d => 1);
 
   // Render the graph with D3 after React has finished mounting the SVG element on the DOM
   useEffect(() => {
-    renderPackedCircleGraph(data, 700, 700);
+    renderPackedCircleGraph(rootNode, 700, 700);
+
+    setTimeout(() => {
+      // TEST dynamically remov nodes from the hierarchy
+      console.log('removing first child', rootNode.children[0]);
+      rootNode.children = rootNode.children.slice(1);
+      renderPackedCircleGraph(rootNode, 700, 700)
+    }, 2000);
   });
 
   return (
     <>
       <h1>Packed Circles</h1>
       <svg width={700} height={700} className="packed-circles-viz">
+        <g></g>
       </svg>
     </>
   );
--- frontend/src/pages/commitmentsViz.js
+++ frontend/src/pages/commitmentsViz.js
@@ -1,7 +1,4 @@
-import React, { useState, useEffect } from 'react';
-import { select, event } from 'd3-selection';
-import { pack, hierarchy } from 'd3-hierarchy';
-import { zoom } from 'd3-zoom';
+import React from 'react';
 
 import Layout from '../components/layout';
 import PackedCircles from '../components/visualization/PackedCircles';
@@ -10,8 +7,15 @@ import BubbleChart from '../components/visualization/BubbleChart';
 // TODO
 // - [X] Basic circle packing visualization
 // - [ ] Display labels
+// - Dynamically aggregate commitments at higher zoom levels, and render more granualar when zoomed in
 // - [ ] Dynamically set the visualization dimensions based on screen dimensions
 // - [ ] Use real commitments data from server instead of dummy data
+
+// Interesting d3 examples
+//  - Position transition animations: https://jsfiddle.net/VividD/WDCpq/8/
+//  - Canvas packed circles with additional detail rendering on zoom:
+//    - Overview: https://gist.github.com/nbremer/667e4df76848e72f250b
+//    - Demo: https://nbremer.github.io/occupationscanvas/
 
 export default () => {
   return (
