fa32c58b0fc44541b580a4fc691e8f57312340f2
Charlie Holtz <charlieholtz@gmail.com>
2020-06-14 18:59:15 -0400

    new font / text

--- frontend/src/commitment_components/Welcome.js
+++ frontend/src/commitment_components/Welcome.js
@@ -5,11 +5,13 @@ import Button from "react-bootstrap/Button"
 const Welcome = ({ currentStep, stepVal, onClick }) => {
   return currentStep !== stepVal ? null : (
     <div className="text-center">
-      <h1>The Climate Museum</h1>
-      <h2 className="lead">
-        Welcome to Climate Commitments. The first The first museum in the U.S.
-        dedicated to the climate crisis.
-      </h2>
+      <h1>Take Climate Action</h1>
+      <br />
+      <p>
+        Want to take action on climate change but don't know where to start?
+        We've compiled 5 <b>meaningful</b> actions you can take alongside
+        others. Start taking collective action now.
+      </p>
       <Button className="mt-4" onClick={onClick}>
         {" "}
         Get started
