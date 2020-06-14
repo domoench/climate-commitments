ce36d632a4de349e9ff0eedfd5d64f8fb2d1bf12
Charlie Holtz <charlieholtz@gmail.com>
2020-06-14 19:11:37 -0400

    cleaned up navbar

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -1,17 +1,17 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
-import logo from "../images/logo1.png"
+import logo from "../images/logo.png"
 import Navbar from "react-bootstrap/Navbar"
 import Nav from "react-bootstrap/Nav"
 import Image from "react-bootstrap/Image"
 const Header = ({ siteTitle }) => (
-  <Navbar bg="light" className="justify-content-center mb-5">
+  <Navbar className="justify-content-center mb-5">
     <Link to="/">
       <Navbar.Brand>
         <Image
           src={logo}
-          style={{ height: "50px", marginBottom: "10px" }}
+          style={{ height: "100px", marginBottom: "10px" }}
         ></Image>
       </Navbar.Brand>
     </Link>
