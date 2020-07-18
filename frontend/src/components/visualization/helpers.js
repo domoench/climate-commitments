f0ebdd5327310efe891b5ef68b1c25a524f3ee8f
David Moench <domoench@gmail.com>
2020-07-18 18:02:38 -0400

    Remove old dummy data generators.

--- frontend/src/components/visualization/helpers.js
+++ frontend/src/components/visualization/helpers.js
@@ -2,66 +2,9 @@ import { v4 as uuidv4 } from 'uuid';
 import names from './names';
 import { COMMITMENT_TYPES } from '../../commitments';
 
-// Generate dummy hierarchichal data
-// Hierarchy: Country -> Postal Code -> Commitment Type -> Individual commitments
-export const generateHierarchicalData = () => {
-  const countryCodes = ['US', 'CA', 'CN', 'TW', 'IN', 'GL'];
-  const postalCodes = ['pc_1', 'pc_2', 'pc_3', 'pc_4', 'pc_5', 'pc_6', 'pc_7', 'pc_8', 'pc_9', 'pc_10', 'pc_11', 'pc_12', 'pc_13'];
-  const commitments = ['commitment_1', 'commitment_2', 'commitment_3', 'commitment_4', 'commitment_5'];
-
-  const generateCountryNode = (countryCode) => {
-    const randomPostalCodes = postalCodes.filter(pc => Math.random() > 0.6);
-
-    const children = randomPostalCodes.map(pc => generatePostalNode(pc, countryCode));
-    return {
-      id: uuidv4(),
-      name: countryCode,
-      children,
-    };
-  };
-
-  const generatePostalNode = (postalCode, countryCode) => {
-    // TODO this can lead to non-leaf nodes with no children
-    const randomCommitments = commitments.filter(pc => Math.random() > 0.8);
-
-    const children = randomCommitments.map(c => generateCommitmentNode(c, countryCode));
-    return {
-      id: uuidv4(),
-      name: postalCode,
-      children,
-    };
-  }
-
-  const generateCommitmentNode = (commitment, countryCode) => {
-    const numCommitments = countryCode === 'US' ?
-      Math.max(1, Math.ceil(Math.random() * 12)) :
-      Math.max(1, Math.ceil(Math.random() * 500));
-    const commitments = new Array(numCommitments);
-    for (let i = 0; i < numCommitments; ++i) {
-      commitments[i] = {
-        id: uuidv4(),
-        name: generateName(),
-      };
-    }
-
-    return {
-      id: uuidv4(),
-      name: commitment,
-      children: commitments,
-    };
-  }
-
-  return {
-    id: uuidv4(),
-    name: 'earth',
-    children: countryCodes.map(cc => generateCountryNode(cc)),
-  };
-};
-
 const randomElem = (arr) => arr[Math.ceil(Math.random() * 100) % arr.length]
 const randomBool = () => randomElem([true, false]);
 
-// TODO delete other (flat and hierarchichal) data generators
 // Generate mock raw commitments data array
 export const generateData = (n) => {
   const countryCodes = ['US', 'CA', 'CN', 'TW', 'IN', 'GL'];
@@ -157,38 +100,6 @@ const buildChildren = (parentName, parentDepth, subtreeLeaves, hierarchyKeys) =>
     }
   ));
 }
-
-/*
- * TREE NODE HELPERS
- */
-// TODO
-
-// Generate flat hierarchichal data with n elements
-export const generateFlatData = (n) => {
-  const countryCodes = ['US', 'CA', 'CN', 'TW', 'IN', 'GL'];
-  const postalCodes = ['pc_1', 'pc_2', 'pc_3', 'pc_4', 'pc_5', 'pc_6', 'pc_7', 'pc_8', 'pc_9', 'pc_10', 'pc_11', 'pc_12', 'pc_13'];
-  const commitmentTypes = ['commitment_1', 'commitment_2', 'commitment_3', 'commitment_4', 'commitment_5'];
-
-  const randomElem = (arr) => arr[Math.ceil(Math.random() * 100) % arr.length]
-
-  const numCommitments = n;
-  const commitments = new Array(numCommitments);
-
-  for (let i = 0; i < numCommitments; ++i) {
-    commitments[i] = {
-      name: generateName(),
-      id: uuidv4(),
-      commitment: randomElem(commitmentTypes),
-      country: randomElem(countryCodes),
-      postalCode: randomElem(postalCodes),
-    };
-  }
-
-  return {
-    id: uuidv4(),
-    children: commitments,
-  };
-};
 
 // Generates the next color in the sequence, going from 0,0,0 to 255,255,255.
 // From: https://bocoup.com/weblog/2d-picking-in-canvas
